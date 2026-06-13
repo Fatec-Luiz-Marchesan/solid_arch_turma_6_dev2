87cc18262a62e568e9995ff5c5e7287c42c5e365
Fernanda Garcia de Oliveira <fegaolive@gmail.com>
2026-06-13 17:51:16 -0300

    correção

--- backend/useCases/vaccine/UpdateVaccineUseCase.js
+++ backend/useCases/vaccine/UpdateVaccineUseCase.js
@@ -1,17 +1,35 @@
-
-
-const Vaccine = require("../../models/Vaccine");
+const Vaccine = require('../../models/Vaccine')
 
 class UpdateVaccineUseCase {
+  async execute(id, data) {
+    const allowedFields = [
+      'name',
+      'description',
+      'laboratory',
+      'doses',
+      'interval',
+      'available',
+    ]
 
-    async execute(id, data) {
+    const filteredData = {}
 
-        return await Vaccine.findByIdAndUpdate(
-            id,
-            data,
-            { new: true }
-        );
-    }
+    allowedFields.forEach((field) => {
+      if (data[field] !== undefined) {
+        filteredData[field] = data[field]
+      }
+    })
+
+    return await Vaccine.findByIdAndUpdate(
+      id,
+      {
+        $set: filteredData,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
+  }
 }
 
-module.exports = UpdateVaccineUseCase;
+module.exports = UpdateVaccineUseCase
